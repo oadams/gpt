d35fd94cfd87b1177c41f6043413485d1518bc84
Oliver Adams <oliver.adams@gmail.com>
2024-06-18 09:56:56 +1000

    Bugfix and refactor.

--- tensor.py
+++ tensor.py
@@ -7,6 +7,23 @@ class GradError(Exception):
     pass
 
 
+def dl_dr_check(dl_dr, result_data):
+    if dl_dr is None:
+        if len(result_data.shape) == 0:
+            dl_dr = Tensor(1)
+        elif len(result_data.shape) == 1 and result_data.shape[0] == 1:
+            dl_dr = Tensor([1])
+        else:
+            raise GradError(
+                "If tensor is not scalar then backwards must be called with a gradient of the same shape."
+            )
+    if dl_dr.data.shape != result_data.shape:
+        raise GradError(
+            "dL_dthis isn't of the right shape. It needs to match the shape of the tensor we're calling backward on."
+        )
+    return dl_dr
+
+
 class Tensor:
     def __init__(
         self, data, dtype=torch.float32, device="cpu", requires_grad=False, is_leaf=True
@@ -46,19 +63,7 @@ class Tensor:
             - dl/dself = dl/dr * dr/dself: Each element of self can only affect the loss via the corresponding element of r, so we just take the hadamard
             product of dl/dr with the ones tensor, resulting in dl/dr tensor again.
             """
-            if dl_dr is None:
-                if len(result_data.shape) == 0:
-                    dl_dr = Tensor(1)
-                elif len(result_data.shape) == 1 and result_data.shape[0] == 1:
-                    dl_dr = Tensor([1])
-                else:
-                    raise GradError(
-                        "If tensor is not scalar then backwards must be called with a gradient of the same shape."
-                    )
-            if dl_dr.data.shape != result_data.shape:
-                raise GradError(
-                    "dL_dthis isn't of the right shape. It needs to match the shape of the tensor we're calling backward on."
-                )
+            dl_dr = dl_dr_check(dl_dr, result_data)
             # TODO Explain in clearer terms below how we arrived at this
             # gradient computation clearly. The clarity of the documentation and
             # story here is as important as the implementation.  The reason
@@ -94,19 +99,7 @@ class Tensor:
         result = Tensor(result_data, requires_grad=self.requires_grad, is_leaf=False)
 
         def backward(dl_dr=None):
-            if dl_dr is None:
-                if len(result_data.shape) == 0:
-                    dl_dr = Tensor(1)
-                elif len(result_data.shape) == 1 and result_data.shape[0] == 1:
-                    dl_dr = Tensor([1])
-                else:
-                    raise GradError(
-                        "If tensor is not scalar then backwards must be called with a gradient of the same shape."
-                    )
-            if dl_dr.data.shape != result_data.shape:
-                raise GradError(
-                    "dL_dthis isn't of the right shape. It needs to match the shape of the tensor we're calling backward on."
-                )
+            dl_dr = dl_dr_check(dl_dr, result_data)
             if self.requires_grad:
                 # The elementwise product with ones is to account for a `dim` argument having collapsed the matrix along a given dimension.
                 # We just broadcast the dl_dr appropriately to fit the shape of the original matrix.
@@ -115,11 +108,14 @@ class Tensor:
                     self_grad = dl_dr.data * torch.ones_like(
                         self.data, requires_grad=False
                     )
-                else:
+                elif dim:
                     # Then we need to unsqueeze dl_dr because dl_dr would have lost a dimension in order to match the shape of the result.
                     self_grad = dl_dr.data.unsqueeze(dim=dim) * torch.ones_like(
                         self.data, requires_grad=False
                     )
+                else:
+                    # Then dl_dr needs to match the shape of result, so we just have
+                    self_grad = dl_dr.data
                 if self.is_leaf:
                     if self.grad is not None:
                         self.grad = self.grad + self_grad
@@ -141,20 +137,7 @@ class Tensor:
         )
 
         def backward(dl_dr=None):
-            if dl_dr is None:
-                if len(result_data.shape) == 0:
-                    dl_dr = Tensor(1)
-                elif len(result_data.shape) == 1 and result_data.shape[0] == 1:
-                    dl_dr = Tensor([1])
-                else:
-                    raise GradError(
-                        "If tensor is not scalar then backwards must be called with a gradient of the same shape."
-                    )
-            if dl_dr.data.shape != result_data.shape:
-                raise GradError(
-                    "dL_dthis isn't of the right shape. It needs to match the shape of the tensor we're calling backward on."
-                )
-
+            dl_dr = dl_dr_check(dl_dr, result_data)
             if self.requires_grad:
                 self_grad = dl_dr.data @ other.data.T
                 if self.is_leaf:
